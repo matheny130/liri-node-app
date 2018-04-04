a31f68579bc9f0a558b2b1af5315c8c30e03a327
Mike Matheny <mike.matheny.81@gmail.com>
2018-04-04 19:59:30 -0400

    inital commit

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.cxaDO1WHVbXiOq3aTKZ9JSXRK,
+  consumer_secret: process.env.swNRmpQUeCLhQ2aIZhdQ5x3WXWrRrf5sXaBgOMrguKL1HKhluZ
+,
+  access_token_key: process.env.981657455066255366-CmEiQYINYwIr87AG4GFQJ7ZOjj29D2q,
+  access_token_secret: process.env.1GoHU7ZJIElWiG9tl0yTXQbRsLPdF68XDi5pPMPpRE7Zg
+};
+
+exports.spotify = {
+  id: process.env.e4db7ca7ad8c4e469fac212e5debfa35,
+  secret: process.env.40fd0a990a474f328f37c49a52e9857f
+};
